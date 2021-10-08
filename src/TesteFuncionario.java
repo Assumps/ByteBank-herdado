e49d87ad012a26cb91091a287ec1d87713c40c20
Assumps <jrvitor5@gmail.com>
2021-10-08 19:43:20 -0300

    Initial commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setNome("Joao Vitor");
+        funcionario.setCpf("487-695-888-21");
+        funcionario.setSalario(4500.0);
+
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getCpf());
+        System.out.println(funcionario.getSalario());
+        System.out.println(funcionario.getBonificacao());
+
+    }
+}
